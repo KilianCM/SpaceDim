0df33d982183ff2f000bf6abe88b67aea0f9b43c
Kilian Chamiot-Maitral <chamiot.k@gmail.com>
2020-01-10 14:56:07 +0100

    Add navigation between home and game activities

--- app/src/main/java/com/lpdim/spacedim/home/HomeActivity.kt
+++ app/src/main/java/com/lpdim/spacedim/home/HomeActivity.kt
@@ -1,13 +1,23 @@
 package com.lpdim.spacedim.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.lpdim.spacedim.R
+import com.lpdim.spacedim.game.GameActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun launchGame(view: View) {
+        val intent = Intent(this, GameActivity::class.java).apply {
+            //putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
