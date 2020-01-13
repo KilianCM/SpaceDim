a95ee43e143c9f756d378f6ab3254b86b6549c16
Thiebault <thiebault.bebert@outlook.fr>
2020-01-13 17:42:54 +0100

    Request to find user by name

--- app/src/main/java/com/lpdim/spacedim/home/HomeActivity.kt
+++ app/src/main/java/com/lpdim/spacedim/home/HomeActivity.kt
@@ -6,12 +6,21 @@ import android.os.Bundle
 import android.view.View
 import com.lpdim.spacedim.R
 import com.lpdim.spacedim.game.GameActivity
+import com.lpdim.spacedim.game.MoshiService
+import com.lpdim.spacedim.game.WebSocketLiveData
+import com.lpdim.spacedim.game.model.User
+import kotlinx.android.synthetic.main.activity_home.*
+import okhttp3.*
+import timber.log.Timber
+import java.io.IOException
+import java.lang.Exception
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        textBoxName.text
     }
 
     fun launchGame(view: View) {
@@ -20,5 +29,36 @@ class HomeActivity : AppCompatActivity() {
             //putExtra(EXTRA_MESSAGE, message)
         }
         startActivity(intent)
+    }
+
+    fun testIfUserExist(username: String): Int?{
+        var userId: Int? = null
+        val url = "http://vps769278.ovh.net/doc/api/user/find/$textBoxName"
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        WebSocketLiveData.client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if(response.isSuccessful){
+                        try {
+                            val adapter = MoshiService.moshi.adapter(User::class.java)
+                            response.body?.let {
+                                val user = adapter.fromJson(it.source())
+                                userId = user?.id
+                            }
+                        } catch (e: Exception) {
+                            Timber.d("Failed to deserialize user")
+                        }
+                    }
+                }
+            }
+        })
+
+        return userId
     }
 }
