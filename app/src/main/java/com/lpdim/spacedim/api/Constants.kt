80cb5d56a0d096235fbbd40e8cc723524f2ffbfa
Thiebault <thiebault.bebert@outlook.fr>
2020-01-19 14:51:47 +0100

    Request to get the user score

--- app/src/main/java/com/lpdim/spacedim/api/Constants.kt
+++ app/src/main/java/com/lpdim/spacedim/api/Constants.kt
@@ -4,7 +4,7 @@ object API {
     const val BASE_URL_HTTP = "http://vps769278.ovh.net"
     const val REGISTER_USER = "/api/user/register"
     const val GET_USER = "/api/user/find/"
-
+    const val GET_USER_BY_ID = "/api/user/"
     const val BASE_URL_WS = "ws://vps769278.ovh.net:8081/ws"
     const val JOIN_ROOM = "/join/"
 }
--- app/src/main/java/com/lpdim/spacedim/score/ScoreActivity.kt
+++ app/src/main/java/com/lpdim/spacedim/score/ScoreActivity.kt
@@ -2,12 +2,61 @@ package com.lpdim.spacedim.score
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.lpdim.spacedim.R
+import com.lpdim.spacedim.api.API
+import com.lpdim.spacedim.game.MoshiService
+import com.lpdim.spacedim.game.MoshiService.userAdapter
+import com.lpdim.spacedim.game.WebSocketLiveData.Companion.client
+import com.lpdim.spacedim.game.model.User
+import kotlinx.android.synthetic.main.activity_score.*
+import okhttp3.Call
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import javax.security.auth.callback.Callback
+import kotlin.math.absoluteValue
 
 class ScoreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
+        getUserScore(3)
+    }
+
+    private fun getUserScore(userId: Int){
+        val request = Request.Builder()
+            .url(API.BASE_URL_HTTP + API.GET_USER_BY_ID + userId.toString())
+            .build()
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    Toast.makeText(
+                        this@ScoreActivity,
+                        getString(R.string.user_not_exist),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use{
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    response.body?.let {
+                        val user = userAdapter.fromJson(it.source())
+                        val userScore = user?.score
+                        userScore?.let {
+                            runOnUiThread {
+                                tvScore.text = userScore.toString()
+                            }
+                        }
+                    }
+
+                }
+            }
+        })
+
     }
 }
